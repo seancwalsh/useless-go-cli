0b80ba33e51fb62a3f4bd0bb17551f948b5167eb
Sean Walsh <seanwalsh@hey.com>
2023-09-06 15:57:53 +0300

    fix: name of cli

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var out io.Writer = os.Stdout
 func main() {
   if len(os.Args) == 1 {
     fmt.Fprintln(out, "\nError: you must pass your name as an arg into this useless CLI")
-    fmt.Fprintln(out, "\nUsage: useless <name>")
+    fmt.Fprintln(out, "\nUsage: useless-go-cli <name>")
     fmt.Fprintln(out, "")
     return
   }
